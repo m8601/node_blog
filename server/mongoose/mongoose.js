7cca258f1f596a99986bd92144095fedc626471d
m8601 <robert.szymanski08@gmail.com>
2018-01-07 17:50:42 +0100

    Added mockup of index.html page

--- server/mongoose/mongoose.js
+++ server/mongoose/mongoose.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost:27017/blog');
+mongoose.connect('mongodb://localhost:27017/blog');  // w todo_api z kursu jest konfiguracja na heroku i lokalnie, pozniej to zrobie, jak bede robic dodawanie postow/komentarzy
 
 module.exports = { mongoose };
